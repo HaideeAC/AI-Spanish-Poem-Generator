36be652181b7970273ab45546fae1cf0ad36ce89
HÁ <haidee.alvarez.c@gmail.com>
2024-10-08 11:18:03 +0100

    added axios to have access to ai api

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
-function generatePoem(event) {
-  event.preventDefault();
-  let poem = document.querySelector("#poem");
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings:
-      "Recuerde el alma dormida Avive el seso y despierte Contemplando Cómo se pasa la vida, Cómo se viene la muerte, Tan callando, Cuán presto se va el placer, Cómo, después de acordado Da dolor, Cómo, a nuestro parecer, Cualquier tiempo pasado,Fue mejor.",
+    strings: response.data.answer,
     autoStart: true,
     delay: 100,
     pauseFor: 1500,
     cursor: "",
   });
 }
-
-let poemFormElement = document.querySelector("#poem-generator-form");
-poemFormElement.addEventListener("submit", generatePoem);
-
-/* 
+function generatePoem(event) {
+  event.preventDefault();
+  let topic = document.querySelector("#user-instructions");
   let apiKey = "9453eocfb302f861c59f1e9f04d3bta4";
   let apiContext =
     "You are a bilingual poet equally fluent in English and Spanish, you would get words in English, translate them to Spanish and write a poem with them";
-  let apiPrompt = `Generate a poem in Spanish using the following topic: ${topic}`;
+  let apiPrompt = `Generate a poem in Spanish using the following topic: ${topic.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${apiPrompt}&context=${apiContext}&key=${apiKey}`;
-  */
+
+  axios.get(apiUrl).then(displayPoem);
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
