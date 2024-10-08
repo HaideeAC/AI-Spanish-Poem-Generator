886264a728c11f0b75d35c84da0e139d90e54b95
HÁ <haidee.alvarez.c@gmail.com>
2024-10-08 16:15:04 +0100

    improve user esperience, add blink for the waiting message

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ function displayPoem(response) {
   new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
-    delay: 80,
+    delay: 50,
     pauseFor: 1500,
     cursor: "",
   });
@@ -15,6 +15,10 @@ function generatePoem(event) {
     "You are a bilingual poet equally fluent in English and Spanish, you would get words in English, translate them to Spanish and write a poem with them";
   let apiPrompt = `Generate a poem in Spanish using the following topic: ${topic.value}, don't use more than 150 words`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${apiPrompt}&context=${apiContext}&key=${apiKey}`;
+
+  let showPoem = document.querySelector("#poem");
+  showPoem.classList.remove("hidden");
+  showPoem.innerHTML = `<div class="blink">Generating a Spanish poem about: "${topic.value}"</div>`;
 
   axios.get(apiUrl).then(displayPoem);
 }
